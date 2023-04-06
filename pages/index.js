780eeccd1b02d48e079b9a2d899aec0c826afdc2
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-04-06 13:51:29 +0100

    basic table and data mangling

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,58 @@
-export default function Index() {
-  return <></>;
+export default function Index({ data }) {
+  const processedData = [
+    {
+      name: "France",
+      data: [],
+    },
+    {
+      name: "Spain",
+      data: [],
+    },
+    {
+      name: "Norway",
+      data: [],
+    },
+  ];
+
+  data.forEach((row) => {
+    processedData[0].data.push({ date: row.date, data: row.data[0].value });
+    processedData[1].data.push({ date: row.date, data: row.data[1].value });
+    processedData[2].data.push({ date: row.date, data: row.data[2].value });
+  });
+  console.log(processedData);
+
+  return (
+    <>
+      <h1>Cervest Tech Test</h1>
+      <table>
+        <thead>
+          <th>Region Name</th>
+          {data.map((row) => {
+            return <th>{new Date(row.date).toDateString()}</th>;
+          })}
+        </thead>
+        <tbody>
+          {processedData.map((row) => {
+            return (
+              <tr>
+                <td>
+                  <strong>{row.name}</strong>
+                </td>
+                {row.data.map((row) => {
+                  return <td>{row.data}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/rainfall`);
+  const data = await res.json();
+
+  return { props: { data } };
 }
